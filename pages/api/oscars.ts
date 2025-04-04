ae59dd7cf15d13fd2e2368388a9644934693d2dd
tecnobook <danielsantos.correa72@gmail.com>
2025-04-04 03:03:30 -0300

    version5.1

---
+++ pages/api/oscars.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Movie = {
+  title: string;
+  oscars: number;
+};
+
+const topOscarWinners: Movie[] = [
+  { title: 'Ben-Hur (1959)', oscars: 11 },
+  { title: 'Titanic (1997)', oscars: 11 },
+  { title: 'The Lord of the Rings: The Return of the King (2003)', oscars: 11 },
+  { title: 'West Side Story (1961)', oscars: 10 },
+  { title: 'The English Patient (1996)', oscars: 9 },
+  { title: 'The Last Emperor (1987)', oscars: 9 },
+  { title: 'Gigi (1958)', oscars: 9 },
+  { title: 'The Hurt Locker (2008)', oscars: 6 },
+  { title: 'Chicago (2002)', oscars: 6 },
+  { title: 'Gravity (2013)', oscars: 7 },
+];
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Movie[]>
+) {
+  if (req.method === 'GET') {
+    res.status(200).json(topOscarWinners);
+  } else {
+    res.setHeader('Allow', 'GET');
+    res.status(405).end('Method Not Allowed');
+  }
+}
